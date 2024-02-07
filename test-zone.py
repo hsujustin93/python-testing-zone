577be6e3b4c39c7778aa7acaeecbc383afb6f815
hsujustin93 <hsu.justin93@gmail.com>
2024-02-07 13:29:20 -0500

    added a loop to go through numbers

--- test-zone.py
+++ test-zone.py
@@ -1,9 +1,10 @@
 
 def even_odd(num):
-    if num % 2 == 0:
-        print("even")
-    else:
-        print("odd")
+    for n in range(1, num+1):
+        if n % 2 == 0:
+            print(f"The number {n} is even")
+        else:
+            print(f"The number {n} is odd")
 
-number = int(input("Type in a number to check if it's even or odd: "))
+number = int(input("Type in how many numbers to check for even or odd: "))
 even_odd(number)
